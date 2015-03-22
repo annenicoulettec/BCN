87e530c9a55fccb6f34c4d760c655068e31f8753
unknown <annenicoulettecabrera@yahoo.com>
2015-03-22 14:48:14 +0800

    Initial commit

---
+++ BularioCabreraNebria/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace BularioCabreraNebria
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
